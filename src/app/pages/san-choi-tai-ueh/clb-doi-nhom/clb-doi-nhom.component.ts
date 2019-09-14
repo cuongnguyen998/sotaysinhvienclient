c9e65c955a1bb395fa7224165d0f8aba3ceaf589
Nguyễn Ngọc Đăng Khoa <tenban18@gmail.com>
2019-09-14 23:55:30 +0700

    Update rất nhiều

--- src/app/pages/san-choi-tai-ueh/clb-doi-nhom/clb-doi-nhom.component.ts
+++ src/app/pages/san-choi-tai-ueh/clb-doi-nhom/clb-doi-nhom.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-clb-doi-nhom',
-  templateUrl: './clb-doi-nhom.component.html',
-  styleUrls: ['./clb-doi-nhom.component.scss']
+  selector: "app-clb-doi-nhom",
+  templateUrl: "./clb-doi-nhom.component.html",
+  styleUrls: ["./clb-doi-nhom.component.scss"]
 })
 export class ClbDoiNhomComponent implements OnInit {
+  images = [
+    {
+      text: "",
+      image: "../../../../assets/img/SanChoiTaiUeh/ClbDoiNhom/clb_slide_1.png"
+    },
+    {
+      text: "",
+      image: "../../../../assets/img/SanChoiTaiUeh/ClbDoiNhom/clb_slide_2.png"
+    },
+    {
+      text: "",
+      image: "../../../../assets/img/SanChoiTaiUeh/ClbDoiNhom/clb_slide_3.png"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
