4b9f50bc790e98db7cf3eb39e07c8f868d0e8a8c
Nguyễn Ngọc Đăng Khoa <tenban18@gmail.com>
2019-09-26 17:25:16 +0700

    Update footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -11,15 +11,72 @@ export class FooterComponent implements OnInit {
   ListNavbar: RouteInfo[];
   constructor(private router: Router) {}
 
+  Author: string;
+  Quote: string;
+
+  ngDoCheck(): void {
+    this.ListNavbar = ROUTE;
+
+    for (let i = 0; i < this.ListNavbar.length; i++) {
+      var item = this.ListNavbar[i].childrenPath;
+
+      for (let j = 0; j < item.length; j++) {
+        var comparePath = this.ListNavbar[i].path + item[j].path;
+
+        if (this.router.url === "/") {
+          this.Author = "Walter Bagehot";
+          this.Quote =
+            "Niềm vui lớn nhất trên đời là làm những việc mà mọi người nói bạn không thể làm được";
+        }
+
+        if (this.router.url === "/thong-tin-lien-he") {
+          this.Author = "Caton Censeur";
+          this.Quote = "Đừng mua thứ hữu ích mà hãy mua thứ cần thiết";
+        }
+
+        if (comparePath === this.router.url) {
+          this.Author = item[j].author;
+          this.Quote = item[j].quote;
+          break;
+        }
+      }
+    }
+  }
   ngOnInit() {
     this.ListNavbar = ROUTE;
-    this.ListNavbar.map((item, index) => {
-      item.childrenPath.map((route, i) => {
-        var comparePath = item.path + route.path;
-        if (this.router.url === comparePath) {
-          console.log("cc");
+
+    for (let i = 0; i < this.ListNavbar.length; i++) {
+      var item = this.ListNavbar[i].childrenPath;
+
+      for (let j = 0; j < item.length; j++) {
+        var comparePath = this.ListNavbar[i].path + item[j].path;
+
+        if (this.router.url === "/") {
+          this.Author = "Walter Bagehot";
+          this.Quote =
+            "Niềm vui lớn nhất trên đời là làm những việc mà mọi người nói bạn không thể làm được";
         }
-      });
-    });
+
+        if (this.router.url === "/thong-tin-lien-he") {
+          this.Author = "Caton Censeur";
+          this.Quote = "Đừng mua thứ hữu ích mà hãy mua thứ cần thiết";
+        }
+
+        if (comparePath === this.router.url) {
+          this.Author = item[j].author;
+          this.Quote = item[j].quote;
+          break;
+        }
+      }
+    }
+
+    // this.ListNavbar.map((item, index) => {
+    //   item.childrenPath.map((route, i) => {
+    //     var comparePath = item.path + route.path;
+    //     if (this.router.url === comparePath) {
+    //       console.log("cc");
+    //     }
+    //   });
+    // });
   }
 }
--- src/app/web.config.ts
+++ src/app/web.config.ts
@@ -3,6 +3,8 @@ export declare interface RouteInfo {
   childrenPath: Array<RouteInfo>;
   title: string;
   class: string;
+  author: string;
+  quote: string;
 }
 
 export const ROUTE: RouteInfo[] = [
@@ -13,17 +15,24 @@ export const ROUTE: RouteInfo[] = [
         path: "/van-hoa",
         childrenPath: [],
         title: "Văn hóa UEH",
-        class: ""
+        class: "",
+        quote: "Đạo đức là sức mạnh của tâm hồn",
+        author: "Rousseau"
       },
       {
         path: "/co-so",
         childrenPath: [],
         title: "Các cơ sở trực thuộc",
-        class: ""
+        class: "",
+        quote:
+          "Điều chúng ta biết chỉ là một giọt nước. Điều chúng ta không biết mênh mông như đại dương",
+        author: "Einstein"
       }
     ],
     title: "Tổng quan",
-    class: ""
+    class: "",
+    quote: "",
+    author: ""
   },
   {
     path: "/hoc-tap-ueh",
@@ -32,143 +41,199 @@ export const ROUTE: RouteInfo[] = [
         path: "/portal",
         childrenPath: [],
         title: "Portal sinh viên",
-        class: ""
+        class: "",
+        quote: "Để thắng cuộc, bạn cần cả kỹ năng và ý chí",
+        author: "Frank Tyger"
       },
       {
         path: "/email",
         childrenPath: [],
         title: "Email sinh viên",
-        class: ""
+        class: "",
+        quote: "Trong cách học, phải lấy tự học làm cốt",
+        author: "Hồ Chí Minh"
       },
       {
         path: "/the-sinh-vien",
         childrenPath: [],
         title: "Thẻ sinh viên",
-        class: ""
+        class: "",
+        quote: "Ai không biết nghe, tất không biết nói chuyện",
+        author: "Giarardin"
       },
       {
         path: "/elearning",
         childrenPath: [],
         title: "Hệ thống học trực tuyến (Elearning)",
-        class: ""
+        class: "",
+        quote: "Người khôn ngoan là kẻ mà cái gì cũng thấy mới lạ",
+        author: "Khuyết danh"
       },
       {
         path: "/thu-vien",
         childrenPath: [],
         title: "Thư viện UEH",
-        class: ""
+        class: "",
+        quote:
+          "Google can bring you back 100000 answers, a librarian can bring you back the right one",
+        author: "Neil Gaiman"
       },
       {
         path: "/khung-thoi-gian",
         childrenPath: [],
         title: "Khung thời gian đào tạo hàng năm",
-        class: ""
+        class: "",
+        quote:
+          "Học tập có thể định nghĩa như là quá trình ghi nhớ những điều bạn thích",
+        author: "Richard Saul Wurman"
       },
       {
         path: "/thoi-gian-hoc",
         childrenPath: [],
         title: "Thời gian học trong ngày",
-        class: ""
+        class: "",
+        quote:
+          "Cơ hội giống như bình minh, nếu bạn chờ đợi quá lâu, bạn có thể bỏ lỡ nó",
+        author: "William Arthur"
       },
       {
         path: "/dang-ky-hoc-phan",
         childrenPath: [],
         title: "Đăng ký học phần, hủy học phần",
-        class: ""
+        class: "",
+        quote:
+          "Chúng ta không nên bỏ cuộc, chúng ta không nên để những khó khăn đánh bại mình",
+        author: "A.P.J.Abdul Kalam"
       },
       {
         path: "/mo-lop-hoc-phan",
         childrenPath: [],
         title: "Mở lớp học phần theo nhu cầu",
-        class: ""
+        class: "",
+        quote:
+          "Trí tuệ con người trưởng thành trong tĩnh lặng còn tính cách con người trưởng thành trong bão táp",
+        author: "W.Goethe"
       },
       {
         path: "/hoc-phi",
         childrenPath: [],
         title: "Học phí - cách thức nộp học phí",
-        class: ""
+        class: "",
+        quote: "Dẫu có bạc vàng trăm vạn lạng. Chẳng bằng kinh sử một vài pho",
+        author: "Lê Quý Đôn"
       },
       {
         path: "/vang-thi",
         childrenPath: [],
         title: "Vắng thi - Đăng ký thi ghép",
-        class: ""
+        class: "",
+        quote: "Cái gì xuất phát từ trái tim thì sẽ đi đến trái tim",
+        author: "Khuyết danh"
       },
       {
         path: "/phuc-khao",
         childrenPath: [],
         title: "Quy định liên quan về Thi - Phúc khảo",
-        class: ""
+        class: "",
+        quote: "Thành công là việc sử dụng tối đa khả năng mà bạn có",
+        author: "Zig Zilar"
       },
       {
         path: "/danh-gia-ren-luyen",
         childrenPath: [],
         title: "Đánh giá rèn luyện sinh viên",
-        class: ""
+        class: "",
+        quote:
+          "Không có hoàn cảnh nào tuyệt vọng, chỉ có người tuyệt vọng vì hoàn cảnh",
+        author: "Khuyết danh"
       },
       {
         path: "/chuyen-diem",
         childrenPath: [],
         title: "Chuyển điểm",
-        class: ""
+        class: "",
+        quote: "Thử thách lớn nhất của con người là lúc thành công rực rỡ",
+        author: "G.Welles"
       },
       {
         path: "/nghi-hoc",
         childrenPath: [],
         title: "Nghỉ học tạm thời - Nhập học lại - Thôi học",
-        class: ""
+        class: "",
+        quote: "Đầu tư vào tri thức đem lại lợi nhuận cao nhất",
+        author: "Benjamin Franklin"
       },
       {
         path: "/nghien-cuu",
         childrenPath: [],
         title: "Nghiên cứu khoa học sinh viên",
-        class: ""
+        class: "",
+        quote: "Mọi công việc trọng yếu đều bắt đầu đầy khó nhọc",
+        author: "Spencer"
       },
       {
         path: "/hoc-phan-khoa-luan",
         childrenPath: [],
         title: "Học phần khóa luận tốt nghiệp",
-        class: ""
+        class: "",
+        quote: "Học tập là vấn đề thái độ chứ không phải năng khiếu",
+        author: "Georgi Lozanov"
       },
       {
         path: "/chuan-dau-ra",
         childrenPath: [],
         title: "Chuẩn đầu ra các ngành/ chuyên ngành",
-        class: ""
+        class: "",
+        quote:
+          "Nếu ta không gieo trồng tri thức khi còn trẻ, nó sẽ không cho ta bóng râm khi về già",
+        author: "Lãnh chúa Chesterfield"
       },
       {
         path: "/dieu-kien-tot-nghiep",
         childrenPath: [],
         title: "Điều kiện xét và công nhận tốt nghiệp",
-        class: ""
+        class: "",
+        quote: "Làm điều bạn thích là tự do. Thích điều bạn làm là hạnh phúc",
+        author: "Frank Tyger"
       },
       {
         path: "/xep-hang-tot-nghiep",
         childrenPath: [],
         title: "Xếp hạng tốt nghiệp",
-        class: ""
+        class: "",
+        quote: "ĐNếu cơ hội không gõ cửa, hãy tạo ra một cánh cửa",
+        author: "Milton Berle"
       },
       {
         path: "/chat-luong-cao",
         childrenPath: [],
         title: "Chương trình chất lượng cao",
-        class: ""
+        class: "",
+        quote: "Những gì ta cho đi thật lòng thì mãi mãi là của ta",
+        author: "Georges Granville"
       },
       {
         path: "/cu-nhan-tai-nang",
         childrenPath: [],
         title: "Chương trình cử nhân tài năng",
-        class: ""
+        class: "",
+        quote:
+          "Đừng bao giờ khiêm tốn với kẻ kiêu căng, cũng đừng bao giờ kiêu căng với người khiêm tốn",
+        author: "Thomas Jefferson"
       },
       {
         path: "/dao-tao-thu-hai",
         childrenPath: [],
         title: "Chương trình đào tạo thứ hai",
-        class: ""
+        class: "",
+        quote: "Việc đọc đối với tâm trí cũng như thể dục đối với cơ thể",
+        author: "Joseph Addison"
       }
     ],
     title: "Học tập",
-    class: ""
+    class: "",
+    quote: "",
+    author: ""
   },
   {
     path: "/dong-hanh-ueh",
@@ -177,47 +242,63 @@ export const ROUTE: RouteInfo[] = [
         path: "/co-van-hoc-tap",
         childrenPath: [],
         title: "Cố vấn học tập",
-        class: ""
+        class: "",
+        quote: "Thà học muộn còn hơn không bao giờ học",
+        author: "Publilius Syrus"
       },
       {
         path: "/ky-tuc-xa",
         childrenPath: [],
         title: "Ký túc xá UEH",
-        class: ""
+        class: "",
+        quote: "Thà học muộn còn hơn không bao giờ học",
+        author: "Publilius Syrus"
       },
       {
         path: "/mien-giam-hoc-phi",
         childrenPath: [],
         title: "Chế độ miễn giảm học phí (MGHP)",
-        class: ""
+        class: "",
+        quote: "Thà học muộn còn hơn không bao giờ học",
+        author: "Publilius Syrus"
       },
       {
         path: "/hoc-bong-sinh-vien",
         childrenPath: [],
         title: "Học bổng cho sinh viên",
-        class: ""
+        class: "",
+        quote: "Làm điều bạn thích là tự do. Thích điều bạn làm là hạnh phúc",
+        author: "Frank Tyger"
       },
       {
         path: "/tin-dung-hoc-tap",
         childrenPath: [],
         title: "Tín dụng học tập",
-        class: ""
+        class: "",
+        quote: "Hãy hiền dịu khoan dung với hết mọi người trừ bản thân mình",
+        author: "Joubert"
       },
       {
         path: "/viec-lam",
         childrenPath: [],
         title: "Việc làm",
-        class: ""
+        class: "",
+        quote: "Hãy học khi người khác ngủ; lao động khi người khác lười nhác",
+        author: "William Arthur Ward"
       },
       {
         path: "/khoi-nghiep",
         childrenPath: [],
         title: "Khởi nghiệp",
-        class: ""
+        class: "",
+        quote: "Mắt ta chỉ nhìn thấy những gì mà trí óc sẵn sàng để lĩnh hội",
+        author: "Robertson Davies"
       }
     ],
     title: "Đồng hành cùng sinh viên",
-    class: ""
+    class: "",
+    quote: "",
+    author: ""
   },
   {
     path: "/san-choi-tai-ueh",
@@ -226,35 +307,51 @@ export const ROUTE: RouteInfo[] = [
         path: "/thong-tin-doan-hoi",
         childrenPath: [],
         title: "Thông tin về Đoàn - hội",
-        class: ""
+        class: "",
+        quote:
+          "Một cuốn sách hay cho ta một điều tốt, một người bạn tốt cho ta một điều hay",
+        author: "Gustave Le Bon"
       },
       {
         path: "/clb-doi-nhom",
         childrenPath: [],
         title: "Các Câu lạc bộ/ Đội/ Nhóm/ Ban Chuyên môn",
-        class: ""
+        class: "",
+        quote:
+          "Lý trí có thể mách bảo ta điều phải tránh, còn con tim sẽ chỉ cho ta biết điều phải làm",
+        author: "Joseph Joubert"
       },
       {
         path: "/hoat-dong-doan",
         childrenPath: [],
         title: "Các hoạt động đoàn - hội nổi bật trong năm",
-        class: ""
+        class: "",
+        quote:
+          "Nếu ta không gieo trồng tri thức khi còn trẻ, nó sẽ không cho ta bóng râm khi về già",
+        author: "Lãnh chúa Chesterfield"
       },
       {
         path: "/hoat-dong-van-the-my",
         childrenPath: [],
         title: "Các hoạt động văn thể mỹ dành cho sinh viên đại học chính quy",
-        class: ""
+        class: "",
+        quote:
+          "Nếu ta không gieo trồng tri thức khi còn trẻ, nó sẽ không cho ta bóng râm khi về già",
+        author: "Lãnh chúa Chesterfield"
       }
     ],
     title: "Sân chơi",
-    class: ""
+    class: "",
+    quote: "",
+    author: ""
   },
   {
     path: "/thong-tin-lien-he",
     childrenPath: [],
     title: "Thông tin liên hệ",
-    class: ""
+    class: "",
+    quote: "Đừng mua thứ hữu ích mà hãy mua thứ cần thiết",
+    author: "Caton Censeur"
   },
   {
     path: "/goc-tien-ich",
@@ -263,22 +360,31 @@ export const ROUTE: RouteInfo[] = [
         path: "/bieu-mau",
         childrenPath: [],
         title: "Các biểu mẫu, logo",
-        class: ""
+        class: "",
+        quote:
+          "Nếu ta không gieo trồng tri thức khi còn trẻ, nó sẽ không cho ta bóng râm khi về già",
+        author: "Lãnh chúa Chesterfield"
       },
       {
         path: "/phan-mem-ho-tro",
         childrenPath: [],
         title: "Các phần mềm hỗ trợ trong quá trình học tập của sinh viên",
-        class: ""
+        class: "",
+        quote: "Thà học muộn còn hơn không bao giờ học",
+        author: "Publilius Syrus"
       },
       {
         path: "/xe-buyt",
         childrenPath: [],
         title: "Xe buýt tại TP.HCM - Một số tuyến đi tới các cơ sở của UEH",
-        class: ""
+        class: "",
+        quote: "Tâm hồn phản ánh cuộc sống, diện mạo phản ánh tâm hồn",
+        author: "Balzac"
       }
     ],
     title: "Tiện ích bổ sung",
-    class: ""
+    class: "",
+    quote: "",
+    author: ""
   }
 ];
