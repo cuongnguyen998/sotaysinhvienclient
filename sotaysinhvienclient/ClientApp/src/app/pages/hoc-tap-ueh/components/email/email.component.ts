9d1677229261a5dceffe8fd551d3a34f90f95a34
Nguyen Quoc Cuong <ngquoccuong998@gmail.com>
2020-04-06 21:40:28 +0700

    edit email - remove scss

--- sotaysinhvienclient/ClientApp/src/app/pages/hoc-tap-ueh/components/email/email.component.ts
+++ sotaysinhvienclient/ClientApp/src/app/pages/hoc-tap-ueh/components/email/email.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-email',
-  templateUrl: './email.component.html',
-  styleUrls: ['./email.component.scss']
+  templateUrl: './email.component.html'
 })
 export class EmailComponent implements OnInit {
 
