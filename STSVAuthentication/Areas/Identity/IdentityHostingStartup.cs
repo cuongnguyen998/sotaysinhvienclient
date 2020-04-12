36466b1297f018747f0cf85012c58d83ea968ee3
Nguyen Quoc Cuong <ngquoccuong998@gmail.com>
2020-04-12 19:33:56 +0700

    update nothing important

---
+++ STSVAuthentication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using STSVAuthentication.Data;
+
+[assembly: HostingStartup(typeof(sotaysinhvienclient.Areas.Identity.IdentityHostingStartup))]
+namespace sotaysinhvienclient.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
